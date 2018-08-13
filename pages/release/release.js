4e5f067d522d63e6ce51eee609103a2668d1116a
godxu666666 <37233141+godxu666666@users.noreply.github.com>
2018-08-13 15:33:56 +0800

    666666666666666666666666666

--- pages/release/release.js
+++ pages/release/release.js
@@ -62,7 +62,7 @@ bindTouchEnd: function (e) {
         'content-type': 'application/x-www-form-urlencoded' // 默认值
       },
       success: function(res) {
-        // console.log(res.data);
+        console.log(res.data);
         var data1 = [];
         for (var i = 0; i < res.data.length; i++) {
           data1.push({
